9df2470817f3d385e8eaffe59e827ea96751dcb3
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-11-03 00:17:34 +0800

    Change default theme to system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
