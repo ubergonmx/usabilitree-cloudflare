6def20da815f8cef43fd1bf8a9a3b3b9f68ed550
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-11-02 23:37:01 +0800

    Add header, hero, faq, and footer section & enhanced button UI

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const APP_TITLE = "UsabiliTree";
+export const DATABASE_PREFIX = "usabilitree";
+export const EMAIL_SENDER = '"UsabiliTree" <noreply@usabilitree.com>';
+export const INFINITE_SCROLL_LIMIT = 4;
+export const CODE_LENGTH = 7;
+
+export enum Paths {
+  Home = "/",
+  Login = "/login",
+  Signup = "/signup",
+  AdminDashboard = "/dashboard",
+  VerifyEmail = "/verify-email",
+  ResetPassword = "/reset-password",
+}
