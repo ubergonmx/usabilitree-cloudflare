9c5c6baa6560e84677ea40b634541096abd50232
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-11-05 02:10:30 +0800

    Implement auth frontend

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth/session";
+import { Paths } from "@/lib/constants";
+import { Login } from "./login";
+
+export const metadata = {
+  title: "Login",
+  description: "Login Page",
+};
+
+export const runtime = "edge";
+
+export default async function LoginPage() {
+  const user = await getCurrentUser();
+
+  if (user) redirect(Paths.Dashboard);
+
+  return <Login />;
+}
