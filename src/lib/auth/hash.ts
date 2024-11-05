e37af652e5abd5018c541f5ef0af0621bb87fb1a
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-11-05 14:02:39 +0800

    Refactor password hashing to use base64 encoded salt and improve memory usage

--- src/lib/auth/hash.ts
+++ src/lib/auth/hash.ts
@@ -1,9 +1,10 @@
 import { scrypt, randomBytes } from "node:crypto";
+import { StringDecoder } from "node:string_decoder";
 
 // Define the type for the promisified scrypt function
 const scryptAsync = (
-  password: string | Buffer,
-  salt: string | Buffer,
+  password: string,
+  salt: string,
   keylen: number,
   options: { N: number; r: number; p: number; maxmem: number }
 ): Promise<Buffer> => {
@@ -19,17 +20,17 @@ const scryptConfig = {
   N: 131072, // Cost parameter (higher for better security)
   r: 8, // Block size parameter
   p: 1, // Parallelization parameter
-  maxmem: 134217856, // 128 MB (calculated for N = 131072)
+  maxmem: 135266304, // Maximum memory to use (in bytes) 129MB
 };
 
 export const hash = async (password: string): Promise<string> => {
-  const salt = randomBytes(16);
+  const salt = randomBytes(16).toString("base64");
   const derivedKey = await scryptAsync(password, salt, 32, scryptConfig);
-  return salt.toString("base64") + ":" + derivedKey.toString("base64");
+  return salt + ":" + derivedKey.toString("base64");
 };
 
 export const verify = async (hash: string, password: string): Promise<boolean> => {
   const [salt, key] = hash.split(":");
-  const derivedKey = await scryptAsync(password, Buffer.from(salt, "base64"), 32, scryptConfig);
+  const derivedKey = await scryptAsync(password, salt, 32, scryptConfig);
   return key === derivedKey.toString("base64");
 };
