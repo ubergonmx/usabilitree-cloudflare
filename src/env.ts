8148f384638b4a060680d9f29341bc20ad9f71c4
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-11-08 00:52:43 +0800

    Update tsconfig to use es2017 and refactor env

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,8 @@
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
+
+const { env: cfEnv } = await getCloudflareContext();
 
 export const env = createEnv({
   server: {
@@ -22,23 +25,23 @@ export const env = createEnv({
   },
   client: { NEXT_PUBLIC_APP_URL: z.string().url() },
   runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-    NODE_ENV: process.env.NODE_ENV,
-    CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID,
-    CLOUDFLARE_DATABASE_ID: process.env.CLOUDFLARE_DATABASE_ID,
-    CLOUDFLARE_D1_TOKEN: process.env.CLOUDFLARE_D1_TOKEN,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    SECRET_HASH: process.env.SECRET_HASH,
-    MOCK_SEND_EMAIL: process.env.MOCK_SEND_EMAIL,
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    EMAIL_SENDER: process.env.EMAIL_SENDER,
-    SMTP_HOST: process.env.SMTP_HOST,
-    SMTP_PORT: parseInt(process.env.SMTP_PORT ?? ""),
-    SMTP_USER: process.env.SMTP_USER,
-    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    NEXT_PUBLIC_APP_URL: cfEnv.NEXT_PUBLIC_APP_URL,
+    NODE_ENV: cfEnv.NODE_ENV,
+    CLOUDFLARE_ACCOUNT_ID: cfEnv.CLOUDFLARE_ACCOUNT_ID,
+    CLOUDFLARE_DATABASE_ID: cfEnv.CLOUDFLARE_DATABASE_ID,
+    CLOUDFLARE_D1_TOKEN: cfEnv.CLOUDFLARE_D1_TOKEN,
+    DISCORD_CLIENT_ID: cfEnv.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: cfEnv.DISCORD_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: cfEnv.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: cfEnv.GOOGLE_CLIENT_SECRET,
+    SECRET_HASH: cfEnv.SECRET_HASH,
+    MOCK_SEND_EMAIL: cfEnv.MOCK_SEND_EMAIL,
+    SENDGRID_API_KEY: cfEnv.SENDGRID_API_KEY,
+    EMAIL_SENDER: cfEnv.EMAIL_SENDER,
+    SMTP_HOST: cfEnv.SMTP_HOST,
+    SMTP_PORT: parseInt(cfEnv.SMTP_PORT ?? ""),
+    SMTP_USER: cfEnv.SMTP_USER,
+    SMTP_PASSWORD: cfEnv.SMTP_PASSWORD,
   },
   emptyStringAsUndefined: true,
 });
